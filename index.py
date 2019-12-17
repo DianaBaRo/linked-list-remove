2811e67a826155d3e9bbb7926206cade3cc9f400
Diana Badas <diana.badas@gmail.com>
2019-12-17 17:11:00 +0000

    remove

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
+#remove
+def remove (self, data):
+    #if list is empty
+    if self.head is None:
+        return;
 
+    self.size = self.size - 1;
+
+    currentNode = self.head;
+    previousNode = None;
+
+    while currentNode.data != data:
+        previousNode = currentNode;
+        currentNode = currentNode.nextNode;
+
+    if previousNode is None:
+        self.head = currentNode.nextNode;
+    else:
+        previousNode.nextNode = currentNode.nextNode;
+    
